7f18526c55f8bd4df1d35a30445b6966f8a4b6a6
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-06-08 22:29:34 -0500

    Feat: Iniciando clase principal.

---
+++ src/entrega4/ClasePrincipal.java
@@ -0,0 +1,12 @@
+package entrega4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class ClasePrincipal {
+    private Map<String, List<Animal>>clasificar;
+    private List<Animal>animales;
