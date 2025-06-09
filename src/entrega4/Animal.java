5085f05d183c305b2a5e7fcbf39e1b6ca01b46b7
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-06-08 22:18:00 -0500

    Feat: creación de metodos getters and setters.

--- src/entrega4/Animal.java
+++ src/entrega4/Animal.java
@@ -1,4 +1,4 @@
-clase animal.
+
 
 
 package entrega4;
@@ -14,3 +14,32 @@ public Animal(String nombre, String tipo, String genero){
         this.tipo = tipo;
         this.genero = genero;
     }
+public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    void add(Animal animal) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
