9d21839899c9362054e0351bd3e4413fe62eb9ae
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-06-08 23:09:03 -0500

    Feat: Creando un bucle "do while"

--- src/entrega4/ClasePrincipal.java
+++ src/entrega4/ClasePrincipal.java
@@ -19,3 +19,40 @@ public class ClasePrincipal {
  public void agregarAnimales(){
         Scanner sc = new Scanner(System.in);
         String opcion;
+        
+ do{
+            System.out.println("Ingrese el nombre del animal");
+            String nombre = sc.nextLine();
+            
+            String tipo;
+            
+            do{
+                System.out.println("Ingrese que tipo de animal es: Terrestre, Aereo o Acuatico");
+                tipo = sc.nextLine().toLowerCase();
+                if(!tipo.equals("Terrestre")&& !tipo.equals("Aereo")&& !tipo.equals("Acuatico")){
+                    System.out.println("Tipo de animal no valido, por favor ingrese uno valido"); 
+                }
+            } 
+            while (!tipo.equals("Terrestre") && !tipo.equals("Aereo") && !tipo.equals("Acuatico"));
+            
+            String genero;
+            
+            do{
+                System.out.println("Ingrese el genero del animal:->Femenino o Masculino");
+                
+                genero = sc.nextLine().toLowerCase();
+                if(!genero.equals("Femenino") && !genero.equals("Masculino")){
+                    System.out.println("Genero no valido, por favor ingrese un genero valido");
+                    
+                }
+            }
+            while (!genero.equals("Femenino") && !genero.equals("Masculino"));
+            
+            Animal animal = new Animal(nombre,tipo,genero);
+            animal.add(animal);
+          
+            
+        }
+    }
+    
+}
