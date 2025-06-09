cbb65d6b192c083ca6834abe106cad62a055eddd
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-06-08 22:42:36 -0500

    Feat: Creación del constructor.

--- src/entrega4/ClasePrincipal.java
+++ src/entrega4/ClasePrincipal.java
@@ -10,3 +10,9 @@ import java.util.Scanner;
 public class ClasePrincipal {
     private Map<String, List<Animal>>clasificar;
     private List<Animal>animales;
+    
+ public  ClasePrincipal(){
+        this.clasificar=new HashMap<>();
+        this.animales = new ArrayList<>();
+        
+    }
