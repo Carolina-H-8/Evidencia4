299d4f82fc511947fc5a88be93981950a1674348
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-06-09 23:03:08 -0500

    Feat: Se crea un arreglo para almacenar las personas y un scanner para ingresar por consola.

--- src/entrega4/PrincipalPersona.java
+++ src/entrega4/PrincipalPersona.java
@@ -1,8 +1,19 @@
 
 package entrega4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class PrincipalPersona {
-    
-    
+    public static void main(String[] args) {
+        List<Persona> personas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- Ingreso de Personas ---");
+        System.out.println("Ingresa 'fin' en el nombre para terminar.");
+
+        String nombrePersona;
+    }
 }
